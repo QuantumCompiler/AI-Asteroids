2c86f8c55504173b044c69e68fb41de0e49da868
Taylor Larrechea <tjlarrechea@gmail.com>
2024-07-23 19:11:21 -0700

    Main Game Commit

---
+++ lib/main.py
@@ -0,0 +1,9 @@
+import gymnasium as gym # type: ignore
+env = gym.make("ALE/Asteroids-v5", render_mode="human")
+observation, info = env.reset(seed=42)
+for _ in range(1000):
+    action = env.action_space.sample()  # this is where you would insert your policy
+    observation, reward, terminated, truncated, info = env.step(action)
+    if terminated or truncated:
+        observation, info = env.reset()
+env.close()
